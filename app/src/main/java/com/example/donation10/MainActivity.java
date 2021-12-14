bb7009c4f24c88ad30f93cb88227f46e03598d37
hoangvando <19020251@vnu.edu.vn>
2021-12-14 15:39:28 +0700

    Update: Bài thực hành 2 19020251

--- app/src/main/java/com/example/donation10/MainActivity.java
+++ app/src/main/java/com/example/donation10/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.donation10;
+/**
+ * Author: Hoàng Văn Đô 19020251
+ * Thực hành mobile: Bài 2
+ */
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button donateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action",
+                        Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        donateButton = (Button) findViewById(R.id.donateButton);
+        if (donateButton != null)
+        {
+            Log.v("Donate", "Really got the donate button");
+        }
     }
 }
