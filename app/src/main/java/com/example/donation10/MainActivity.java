c3a1ba5f5520ecee56a0cc4b0da4574cc70e0e41
hoangvando <19020251@vnu.edu.vn>
2021-12-14 15:56:22 +0700

    Update: Bài thực hành 3 19020251

--- app/src/main/java/com/example/donation10/MainActivity.java
+++ app/src/main/java/com/example/donation10/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.donation10;
 /**
  * Author: Hoàng Văn Đô 19020251
- * Thực hành mobile: Bài 2
+ * Thực hành mobile: Bài 3
  */
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +10,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -17,12 +20,16 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
 
     private Button donateButton;
+    private RadioGroup paymentMethod;
+    private ProgressBar progressBar;
+    private NumberPicker amountPicker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,5 +44,16 @@ public class MainActivity extends AppCompatActivity {
         {
             Log.v("Donate", "Really got the donate button");
         }
+        paymentMethod = (RadioGroup) findViewById(R.id.paymentMethod);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        amountPicker = (NumberPicker) findViewById(R.id.amountPicker);
+
+        amountPicker.setMinValue(0);
+        amountPicker.setMaxValue(1000);
+    }
+
+    public void donateButtonPressed(View view)
+    {
+        Log.v("Donate", "Donate Pressed!");
     }
 }
